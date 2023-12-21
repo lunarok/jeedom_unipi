bce8270d935a28dddb668afa2ed4d13050177d8a
Cédric Dupont <lunarok@gmail.com>
2023-12-21 16:36:41 +0100

    Update unipi.py

--- resources/unipi.py
+++ resources/unipi.py
@@ -1,22 +1,20 @@
 import websocket
-import thread
-import time
 import sys
 import json
-import urllib
+import urllib.request
 
 try:
-	addr=sys.argv[1]
-	unipi = 'ws://' + addr + '/ws'
-except :
-	print 'Il faut donner un unipi'
-	exit(3)
-	
+    addr = sys.argv[1]
+    unipi = 'ws://' + addr + '/ws'
+except IndexError:
+    print('Il faut donner un unipi')
+    exit(3)
+
 try:
-	jeedom=sys.argv[2]
-except :
-	print 'Il faut donner un jeedom'
-	exit(3)
+    jeedom = sys.argv[2]
+except IndexError:
+    print('Il faut donner un jeedom')
+    exit(3)
 
 def on_message(ws, message):
     obj = json.loads(message)
@@ -24,25 +22,21 @@ def on_message(ws, message):
     circuit = obj['circuit'] 
     value = obj['value']
     appel = '{jeedom}&type=unipi&messagetype=saveValue&addr={addr}&id={dev}{circuit}&value={value}'.format(jeedom=jeedom, addr=addr, dev=dev, circuit=circuit, value=value)
-    #print appel
-    opener = urllib.FancyURLopener({})
-    f = opener.open(appel)
-    f.close()
+    # print(appel)
+    with urllib.request.urlopen(appel) as f:
+        f.read()
 
 def on_error(ws, error):
-    print error
+    print(error)
 
-def on_close(ws):
-    print "### closed ###"
-
-
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
 
 if __name__ == "__main__":
-
-    #websocket.enableTrace(True)
+    # websocket.enableTrace(True)
     ws = websocket.WebSocketApp(unipi,
-                                on_message = on_message,
-                                on_error = on_error,
-                                on_close = on_close)
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
 
     ws.run_forever()
